385eac497c39ccc3eb709bea31565b2d32612d97
escoand <escoand@users.noreply.github.com>
2012-02-11 12:34:18 +0100

    add id and autoindent

--- src/main.h
+++ src/main.h
@@ -23,7 +23,7 @@
 #define MAIN_H
 
 #define NAME    "socksswitch"
-#define VERSION "20120208"
+#define VERSION "$Id$"
 #define DESC    "forwards socks connection on basis of filter rules"
 #define RIGHTS  "Copyright (C) 2011-2012 Andreas Schoenfelder"
 
